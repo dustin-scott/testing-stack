9376f6115d8b53fbdd4bb8b6eda1851c19f0596f
Dustin Scott <dustin.k.scott@gmail.com>
2025-05-09 00:40:13 -0400

    A Bit late, but I guess laravel does not give an SPA, maybe worth changing later, but rolling with it to get this closer tomorrow.

--- game-backlog/resources/js/pages/Dashboard.vue
+++ game-backlog/resources/js/pages/Dashboard.vue
@@ -3,6 +3,9 @@ import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
 import PlaceholderPattern from '../components/PlaceholderPattern.vue';
+import { Icon } from '@iconify/vue'
+import { PaginationEllipsis, PaginationFirst, PaginationLast, PaginationList, PaginationListItem, PaginationNext, PaginationPrev, PaginationRoot } from 'reka-ui'
+
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -32,5 +35,48 @@ const breadcrumbs: BreadcrumbItem[] = [
                 <PlaceholderPattern />
             </div>
         </div>
+      <PaginationRoot
+        :total="100"
+        :sibling-count="1"
+        :items-per-page="10"
+        show-edges
+        :default-page="2"
+      >
+        <PaginationList
+          v-slot="{ items }"
+          class="flex items-center gap-1 text-stone-700 dark:text-white"
+        >
+          <PaginationFirst class="w-9 h-9  flex items-center justify-center bg-transparent hover:bg-white dark:hover:bg-stone-700/70 transition disabled:opacity-50 rounded-lg">
+            <Icon icon="radix-icons:double-arrow-left" />
+          </PaginationFirst>
+          <PaginationPrev class="w-9 h-9  flex items-center justify-center bg-transparent hover:bg-white dark:hover:bg-stone-700/70 transition mr-4  disabled:opacity-50 rounded-lg">
+            <Icon icon="radix-icons:chevron-left" />
+          </PaginationPrev>
+          <template v-for="(page, index) in items">
+            <PaginationListItem
+              v-if="page.type === 'page'"
+              :key="index"
+              class="w-9 h-9 border dark:border-stone-800 rounded-lg data-[selected]:!bg-white data-[selected]:shadow-sm data-[selected]:text-blackA11 hover:bg-white dark:hover:bg-stone-700/70 transition"
+              :value="page.value"
+            >
+              {{ page.value }}
+            </PaginationListItem>
+            <PaginationEllipsis
+              v-else
+              :key="page.type"
+              :index="index"
+              class="w-9 h-9 flex items-center justify-center"
+            >
+              &#8230;
+            </PaginationEllipsis>
+          </template>
+          <PaginationNext class="w-9 h-9  flex items-center justify-center bg-transparent hover:bg-white dark:hover:bg-stone-700/70 transition ml-4 disabled:opacity-50 rounded-lg">
+            <Icon icon="radix-icons:chevron-right" />
+          </PaginationNext>
+          <PaginationLast class="w-9 h-9  flex items-center justify-center bg-transparent hover:bg-white dark:hover:bg-stone-700/70 transition disabled:opacity-50 rounded-lg">
+            <Icon icon="radix-icons:double-arrow-right" />
+          </PaginationLast>
+        </PaginationList>
+      </PaginationRoot>
     </AppLayout>
 </template>
